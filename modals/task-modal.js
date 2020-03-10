1f549ea3160e4a6fd1be8e491afce49f51c6bc66
vy3191 <venkyafhs@gmail.com>
2020-03-09 20:31:04 -0400

    Completed- GET requst on tasks

--- modals/task-modal.js
+++ modals/task-modal.js
@@ -6,7 +6,23 @@ async function addTasks(newTask) {
    return addedTask;
 };
 
+async function findTasks() {
+  const tasks = await db("tasks as t")
+           .join("projects as p", "p.id", "t.project_id")
+           .select("t.id as Task ID",                  
+                  "p.name as Project Name",             
+                  "p.description as Project Descrition",
+                   "t.project_id as Project ID", 
+                   "t.description as Task Description",
+                   "t.notes as Task Notes",
+                  "t.completed as Is Task Completed?" )
+           .orderBy("t.project_id");
+           
+  return tasks;         
+}
+
 
 module.exports = {
-   addTasks
+   addTasks,
+   findTasks
 }
--- routes/task-route.js
+++ routes/task-route.js
@@ -3,17 +3,30 @@ const router = express.Router();
 const taskModal = require("../modals/task-modal");
 
 router.post("/", async (req,res,next) => {
-    const newTask = {
-      description:req.body.description,
-      notes:req.body.notes,
-      project_id: req.body.project_id,
-      completed:req.body.completed
-    };
-    if(!req.body.description) res.status(400).json({msg:'Description is missing'});
-    if(!req.body.notes) res.status(400).json({msg:'Notes are missing'});
-    if(!req.body.project_id) res.status(400).json({msg:'Project ID is missing.'})
-    const addedTask = await taskModal.addTasks(newTask);
-    res.status(201).json(addedTask);
+    try{
+      const newTask = {
+        description:req.body.description,
+        notes:req.body.notes,
+        project_id: req.body.project_id,
+        completed:req.body.completed
+      };
+      if(!req.body.description) res.status(400).json({msg:'Description is missing'});
+      if(!req.body.notes) res.status(400).json({msg:'Notes are missing'});
+      if(!req.body.project_id) res.status(400).json({msg:'Project ID is missing.'})
+      const addedTask = await taskModal.addTasks(newTask);
+      res.status(201).json(addedTask);
+    }catch(err) {
+      next(err);
+    }
+});
+
+router.get("/", async (req,res,next) => {
+   try{
+     const tasks = await taskModal.findTasks();
+     res.status(200).json(tasks);
+   }catch(err) {
+     next(err);
+   }
 })
 
 module.exports = router;
